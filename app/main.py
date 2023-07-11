d8d5b088459149608a3c430e275f2d4bd9249202
S010MON <leon_debnath@hotmail.co.uk>
2023-07-11 09:43:38 +0100

    Update to make testing work

--- app/main.py
+++ app/main.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
-from .subPackage import func
 import uvicorn
 
 app = FastAPI()
 
-func()
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"msg": "Hello World"}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=5000, reload=True)
