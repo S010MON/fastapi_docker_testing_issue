9fcec4396df7ffa3b78478ffb2eb648a01b219b9
Dog2657 <reeceya2006@gmail.com>
2023-07-11 09:26:40 +0100

    init

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from .subPackage import func
+import uvicorn
 
 app = FastAPI()
 
+func()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=5000, reload=True)
